eb0daff463a19ec7f61106fadd84e0b054b0efc3
ikindiBy <ikindi@mail.ru>
2017-09-14 22:54:08 +0300

    additional_4/fix: string in while

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 module.exports = function multiply(first, second) {
-  let resultNumber =  (+first)*(+second);
- let str = '';
+ 	let resultNumber = (+first)*(+second);
+	let str = '';
  while (resultNumber != 0 ) {
- 	str += resultNumber % 10;
- 	resultNumber = (resultNumber - str)/10;
+ 	let last = resultNumber % 10;
+  	resultNumber = (resultNumber - last)/10;
+ 	str = str + last;
  }
  return str.split('').reverse().join('');
 }
